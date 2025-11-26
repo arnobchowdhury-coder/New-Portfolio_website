62584c462597b62d4be93ab82aaa3676b502a5fb
arnobchowdhury-coder <arnobchy2006@gmail.com>
2025-11-26 22:26:02 +0600

    first commit

---
+++ portfolio_react/src/App.jsx
@@ -0,0 +1,13 @@
+import Home from "./Components/Home/Home"
+import Navbar from "./Components/Navbar/Navbar"
+
+function App() {
+  return (
+    <div className="bg-[#171d32] h-auto w-full overflow-hidden">
+      <Navbar></Navbar>
+      <Home></Home>
+    </div>
+  )
+}
+
+export default App
